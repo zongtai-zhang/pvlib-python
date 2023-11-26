ba9c4fa64da1d2cd3ad64dd40e80a9d60fca7d85
zongtai-zhang <wllf53@durham.ac.uk>
2023-11-26 21:49:38 +0000

    Update testopv.py

--- testopv.py
+++ testopv.py
@@ -14,13 +14,13 @@ from pvlib.ivtools.sdm import fit_desoto
 from pvlib.tools import _first_order_centered_difference
 
 
-v_mp = 4.559 
-i_mp = 1.338 
-v_oc = 5.525 
-i_sc = 1.411  
-alpha_sc = 0.00063495 
-beta_voc = -0.0138125  
-cells_in_series = 1
+v_mp = 46.9 
+i_mp = 4.69 
+v_oc = 59.4
+i_sc = 5.1
+alpha_sc = 0.004539 
+beta_voc = -0.22216  
+cells_in_series = 60
 
 
 fit_desoto(v_mp, i_mp, v_oc, i_sc, alpha_sc, beta_voc, cells_in_series, EgRef=1.121, dEgdT=- 0.0002677, temp_ref=25, irrad_ref=1000, root_kwargs={})
